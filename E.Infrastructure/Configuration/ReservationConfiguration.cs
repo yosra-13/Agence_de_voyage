20d63b557501a7d732ef614bab7bce9e878da0df
yosra-13 <116159761+yosra-13@users.noreply.github.com>
2023-11-20 00:41:39 +0100

    Add files via upload

---
+++ E.Infrastructure/Configuration/ReservationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using E.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E.Infrastructure.Configuration
+{
+    public class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.HasKey(r => new
+            {
+                r.PackFk,
+                r.ClientFk,
+                r.DateReservation
+            });
+           
+        }
+    }
+}
---
+++ E.Infrastructure/ExamenContext.cs
@@ -0,0 +1,45 @@
+﻿using E.ApplicationCore.Domain;
+using E.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E.Infrastructure
+{
+    public class ExamenContext:DbContext
+    {
+        public DbSet<Activite> Activites { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Conseiller> Conseillers { get; set; }
+        public DbSet<Pack> Packs { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+            Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ReservationConfiguration());
+
+
+            modelBuilder.Entity<Client>().HasOne(c => c.Conseiller)
+                .WithMany(l => l.Clients)
+                .HasForeignKey(c => c.ConseillerFk)
+                .OnDelete(DeleteBehavior.Cascade);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>().HaveMaxLength(15);
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
